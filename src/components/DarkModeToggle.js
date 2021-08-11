4c9894ddfd10a479b6ab85db8aca50d9452a2f82
AhmadSaman <ahmad.00079821@gmail.com>
2021-08-11 14:27:16 +0300

    commit tree 7c750b976599813f3ef9ebc641dacf40bac505d1

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,19 +1,20 @@
-import React from "react";
-
-function DarkModeToggle({ theme, setTheme }) {
-  function handleToggleTheme(e) {
-    setTheme(e.target.checked ? "dark" : "light");
-  }
-  return (
-    <label>
-      Dark Mode
-      <input
-        type="checkbox"
-        checked={theme === "dark"}
-        onChange={handleToggleTheme}
-      />
-    </label>
-  );
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme";
+function DarkModeToggle() {
+	const { theme, setTheme } = useContext(ThemeContext);
+	function handleToggleTheme(e) {
+		setTheme(e.target.checked ? "dark" : "light");
+	}
+	return (
+		<label>
+			Dark Mode
+			<input
+				type="checkbox"
+				checked={theme === "dark"}
+				onChange={handleToggleTheme}
+			/>
+		</label>
+	);
 }
 
 export default DarkModeToggle;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Interests from "./Interests";
+import { UserContext } from "../context/user";
 
-function Profile({ user, theme }) {
-  if (!user) return <h2>Please Login To View Profile</h2>;
-  return (
-    <div>
-      <h2>{user.name}'s Profile</h2>
-      <Interests interests={user.interests} theme={theme} />
-    </div>
-  );
+function Profile() {
+	const { user } = useContext(UserContext);
+
+	if (!user) return <h2>Please Login To View Profile</h2>;
+	return (
+		<div>
+			<h2>{user.name}'s Profile</h2>
+			<Interests interests={user.interests} />
+		</div>
+	);
 }
 
 export default Profile;
